42136eab46736966ab23e5acfa4460247b08c705
Rosaline Zhu <shijiezhu@umass.edu>
2022-07-07 13:28:40 -0400

    added saveEvent endpoint

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use('/', express.static('client'));
 
+app.post('/saveEvent', (request, response) => {
+    const {date, startTime, endTime, event } = request.body;
+    await database.saveWordScore(date, startTime, endTime, event );
+    response.status(200).json({ status: 'success' });
+  });
+
 app.listen(port, () => {
 
     const msg = ` Server started on http://localhost:${port}`;
